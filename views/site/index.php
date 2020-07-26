d4413aca516de3f3753e5b2ffc1031648acc6e09
Vladyslav <vopolonc@student.unit.ua>
2020-07-26 14:51:56 +0300

    Grid 10 x 10 with prototype of Modal Ajax

--- views/site/index.php
+++ views/site/index.php
@@ -1,53 +1,69 @@
 <?php
 
-/* @var $this yii\web\View */
+use lo\widgets\modal\ModalAjax;
+use yii\helpers\Url;
 
 $this->title = 'My Yii Application';
+$style = <<<CSS
+
+.flex-container {
+  /* We first create a flex layout context */
+  display: flex;
+  
+  /* Then we define the flow direction 
+     and if we allow the items to wrap 
+   * Remember this is the same as:
+   * flex-direction: row;
+   * flex-wrap: wrap;
+   */
+  flex-flow: nowrap ;
+  
+  /* Then we define how is distributed the remaining space */
+  justify-content: space-around;
+  
+  padding: 0;
+  margin: 0;
+  list-style: none;
+}
+
+.flex-item {
+  background: tomato;
+  padding: 5px;
+  width: 8%;
+  height: 40px;
+  margin-top: 8px;
+  line-height: 40px;
+  color: white;
+  font-weight: bold;
+  font-size: 1em;
+  text-align: center;
+}
+
+CSS;
+
+$this->registerCss($style);
 ?>
-<div class="site-index">
 
-    <div class="jumbotron">
-        <h1>Congratulations!</h1>
+<div class="container">
+    <?php for ($i = 0; $i < 10; $i++): ?>
+        <div class="flex-container">
+            <?php for ($j = 0; $j < 10; $j++): ?>
+                <?php
+                    echo ModalAjax::widget([
+                    'id' => 'createCompany' . $i . '-' . $j,
+                    'header' => 'Create Company',
+                    'toggleButton' => [
+                    'label' => 'New Company',
+                    'class' => 'flex-item'
+                    ],
+                    'url' => Url::current(), // Ajax view with form to load
+                    'ajaxSubmit' => true, // Submit the contained form as ajax, true by default
+                    // ... any other yii2 bootstrap modal option you need
+                    ]);
+               ?>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
-
-    <div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
+            <?php endfor; ?>
         </div>
-
-    </div>
+    <?php endfor; ?>
 </div>
+
